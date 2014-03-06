4eb3ebe614c06ae58c7022e96d4888156b714dd0
sam-adam <samuel.suhendra@gmail.com>
2014-03-06 17:22:31 +0700

    HTTP/ParameterCollection

---
+++ src/Noob/Core/InvalidArgumentTypeException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Noob\Core;
+
+/**
+ * InvalidArgumentTypeException
+ *
+ * @package Noob\Core
+ */
+class InvalidArgumentTypeException extends \InvalidArgumentException {
+    /**
+     * Constructor
+     *
+     * @param string $required
+     * @param mixed $given
+     */
+    public function __construct($required, $given) {
+        $this->message = strtoupper($required).'required, '.gettype($given).' given.';
+    }
+} 
